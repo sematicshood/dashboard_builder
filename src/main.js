543d60f62c738255918b4e0241ac03e5c49e4c7d
Bima Pangestu <bimapangestu280@gmail.com>
2018-12-11 10:20:01 +0700

    add package

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import VeeValidate from 'vee-validate'
+
+Vue.use(Vuex)
+Vue.use(VueRouter)
+Vue.use(VeeValidate)
 
 Vue.config.productionTip = false
 
